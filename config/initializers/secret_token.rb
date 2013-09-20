365012ee1066a0493af610f5c23c830f5a84d05b
Teo Dell'Amico <teo.dellamico@gmail.com>
2013-09-20 14:36:38 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Viadeca::Application.config.secret_key_base = '76849cae3ec4be912e43d0491167c546dc3925a9dff96e2a8dff46b15733a61fb5062886fe41e34328a3a11c5ec3c1bc55501c416522a3433ca4e1258baad558'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Viadeca::Application.config.session_store :cookie_store, key: '_viadeca_session'
